7d1db91971a6061b1b12be104f71b3d7ed1bdb33
AhmadShhadeh <ashhadeh@yahoo.com>
2021-09-04 23:17:41 +0300

    new update

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jul  2 17:59:52 2021
+
+@author: sharukh
 """
 
 """from model import Recommendation
 recommend = Recommendation()
-u='speedlift'
+u='00sab00'
 r=recommend.getTopProducts(u)
 print(r)"""
 
@@ -29,6 +32,6 @@ print(''.join(e+'|||' for e in s))
 
 #from model import Recommendation
 #recommend = Recommendation()
-#u='speedlift'
+#u='00sab00'
 #r=recommend.getUsers()
 #print(r)
